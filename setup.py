1ec09cff56f38755e99689401e8aa618b6843d19
Erfan Abedi <erfanabedi.78@gmail.com>
2023-09-07 00:25:38 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="telegraph-export",
-    version="1.0",
+    version="1.1",
     description="Exporting urls to telegraph",
     author="Random Person",
     author_email="rand@om.",
